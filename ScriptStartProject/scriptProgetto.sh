2fdf76e42b48e9a2799862c891df76256c49dedd
alexiey91 <alexiey@hotmail.it>
2017-06-08 09:45:36 +0200

    Update Script startProject

---
+++ ScriptStartProject/scriptProgetto.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+  echo "************* Start Processing ********************"
+   
+#  echo "PID=`ps -ef | grep usr/local/hadoop/Flume/conf/testFlumePaolo.conf: | grep -v 'grep' | awk '{print $2}'`"
+#	PID=`ps -ef | grep usr/hadoop/Flume/conf/testFlumePaolo.conf: | grep -v 'grep' | awk '{print $2}'`
+
+     
+echo   "/usr/local/hadoop/Flume/bin/flume-ng agent -n agent -c /usr/local/hadoop/Flume/conf -f /usr/local/hadoop/Flume/conf/testFlumePaolo.conf -Dflume.root.looger=DEBUG,console -Xmx2g"
+       /usr/local/hadoop/Flume/bin/flume-ng agent -n agent -c /usr/local/hadoop/Flume/conf -f /usr/local/hadoop/Flume/conf/testFlumePaolo.conf -Dflume.root.looger=DEBUG,console -Xmx2g &
+echo " PID=`ps -ef | grep usr/hadoop/Flume/conf/testFlumePaolo.conf: | grep -v 'grep' | awk '{print $2}'`"	
+	PID=`ps -ef | grep usr/hadoop/Flume/conf/testFlumePaolo.conf: | grep -v 'grep' | awk '{print $2}'` 
+	sleep 9m 
+	 kill -TERM  ${PID} 
+
+echo "/usr/local/hadoop/Flume/bin/flume-ng agent -n agent -c /usr/local/hadoop/Flume/conf -f /usr/local/hadoop/Flume/conf/MoviesFlume.conf -Dflume.root.looger=DEBUG,console -Xmx2g"
+       /usr/local/hadoop/Flume/bin/flume-ng agent -n agent -c /usr/local/hadoop/Flume/conf -f /usr/local/hadoop/Flume/conf/MoviesFlume.conf -Dflume.root.looger=DEBUG,console -Xmx2g &
+	 PID=`ps -ef | grep usr/hadoop/Flume/conf/MoviesFlume.conf: | grep -v 'grep' | awk '{print $2}'`
+        sleep 1m
+         kill -TERM  ${PID}
+ 
+
+ echo "hadoop jar /data/mapreducedesignpatterns.jar query.Query1 hdfs:///SpoolRating/usr/local/hadoop/Flume/SpoolDir hdfs:///SpoolMovies/usr/local/hadoop/Flume/SpoolMovie hdfs:///OutputQuery1"
+       hadoop jar /data/mapreducedesignpatterns.jar query.Query1 hdfs:///SpoolRating/usr/local/hadoop/Flume/SpoolDir hdfs:///SpoolMovies/usr/local/hadoop/Flume/SpoolMovie hdfs:///OutputQuery1
+
+echo "hadoop jar /data/mapreducedesignpatterns.jar query.Query2 hdfs:///SpoolRating/usr/local/hadoop/Flume/SpoolDir hdfs:///SpoolMovies/usr/local/hadoop/Flume/SpoolMovie  hdfs:///OutputQuery2"
+      hadoop jar /data/mapreducedesignpatterns.jar query.Query2 hdfs:///SpoolRating/usr/local/hadoop/Flume/SpoolDir hdfs:///SpoolMovies/usr/local/hadoop/Flume/SpoolMovie  hdfs:///OutputQuery2
+
+echo "hadoop jar /data/mapreducedesignpatterns.jar query.Query3step4 hdfs:///SpoolRating/usr/local/hadoop/Flume/SpoolDir hdfs:///SpoolMovies/usr/local/hadoop/Flume/SpoolMovie  hdfs:///OutputQuery3"
+      hadoop jar /data/mapreducedesignpatterns.jar query.Query3step4 hdfs:///SpoolRating/usr/local/hadoop/Flume/SpoolDir hdfs:///SpoolMovies/usr/local/hadoop/Flume/SpoolMovie  hdfs:///OutputQuery3
+
+
+  echo "************* End Processing  ********************"
+
