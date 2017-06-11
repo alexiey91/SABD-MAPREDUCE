eecaa91f497c430553bdfb54fe826a3580623608
alexiey91 <alexiey@hotmail.it>
2017-06-11 13:40:13 +0200

    Fix Script

--- ScriptStartProject/scriptProgetto.sh
+++ ScriptStartProject/scriptProgetto.sh
@@ -2,14 +2,14 @@
 
   echo "************* Start Processing ********************"
    
-#  echo "PID=`ps -ef | grep usr/local/hadoop/Flume/conf/testFlumePaolo.conf: | grep -v 'grep' | awk '{print $2}'`"
-#	PID=`ps -ef | grep usr/hadoop/Flume/conf/testFlumePaolo.conf: | grep -v 'grep' | awk '{print $2}'`
 
-     
-echo   "/usr/local/hadoop/Flume/bin/flume-ng agent -n agent -c /usr/local/hadoop/Flume/conf -f /usr/local/hadoop/Flume/conf/testFlumePaolo.conf -Dflume.root.looger=DEBUG,console -Xmx2g"
-       /usr/local/hadoop/Flume/bin/flume-ng agent -n agent -c /usr/local/hadoop/Flume/conf -f /usr/local/hadoop/Flume/conf/testFlumePaolo.conf -Dflume.root.looger=DEBUG,console -Xmx2g &
-echo " PID=`ps -ef | grep usr/hadoop/Flume/conf/testFlumePaolo.conf: | grep -v 'grep' | awk '{print $2}'`"	
-	PID=`ps -ef | grep usr/hadoop/Flume/conf/testFlumePaolo.conf: | grep -v 'grep' | awk '{print $2}'` 
+
+
+
+echo   "/usr/local/hadoop/Flume/bin/flume-ng agent -n agent -c /usr/local/hadoop/Flume/conf -f /usr/local/hadoop/Flume/conf/RatingFlume.conf -Dflume.root.looger=DEBUG,console -Xmx2g"
+       /usr/local/hadoop/Flume/bin/flume-ng agent -n agent -c /usr/local/hadoop/Flume/conf -f /usr/local/hadoop/Flume/conf/RatingFlume.conf -Dflume.root.looger=DEBUG,console -Xmx2g &
+echo " PID=`ps -ef | grep usr/hadoop/Flume/conf/RatingFlume.conf: | grep -v 'grep' | awk '{print $2}'`"	
+	PID=`ps -ef | grep usr/hadoop/Flume/conf/RatingFlume.conf: | grep -v 'grep' | awk '{print $2}'` 
 	sleep 9m 
 	 kill -TERM  ${PID} 
 
